aa41b3c43a1a87ffb7dc2b438ce42a27f18dcd28
MaximShishkin <yacyfra@gmail.com>
2022-03-10 22:32:25 +0300

    first commit

---
+++ src/snake/game.java
@@ -0,0 +1,124 @@
+package snake;
+
+public class game {
+	int[][] mas;
+	int napr;
+	int gX, gY;
+	int kol;
+	int new_napr;
+	int dlina;
+	boolean endg;
+	
+	public game()
+	{
+		mas = new int[30][30];
+		
+	}
+	
+	void povorot()
+	{
+		if (Math.abs(new_napr-napr)!= 2) napr = new_napr;
+	}
+	
+	void make_new()
+	{
+		while(true)
+		{
+			int k = (int)(Math.random()*30);
+			int s = (int)(Math.random()*30);
+			if (mas[s][k]==0)
+			{
+				mas[s][k] = -1;
+				break;
+			}
+		}
+	}
+	
+	void start()
+	{
+		for (int i=0; i<30; i++)
+		{
+			for (int j = 0; j<30; j++)
+				{
+					mas[i][j] = 0;
+				}				
+		}
+		
+		
+		napr = 0;
+		kol = 0;
+		mas[14][14] = 1;
+		mas[14][15] = 2;
+		mas[14][16] = 3;
+		
+		dlina = 3;
+		endg = false;
+		make_new();
+		
+	}
+	
+	int peremGoloa()
+	{
+		if (napr==0) //left 
+		{
+			if ((gX - 1) >= 0) gX--;
+			else gX = 29;	
+		}
+		if (napr==1) //up
+		{
+			if ((gX - 1) >= 0) gY--;
+			else gY = 29;	
+		}
+		if (napr==2) //right
+		{
+			if ((gX - 1) >= 0) gX++;
+			else gX = 0;	
+		}
+		if (napr==3) //down
+		{
+			if ((gX - 1) >= 0) gY--;
+			else gY = 0;	
+		}
+		
+		int rez = 0;
+		
+		if (mas[gY][gX]==-1) rez = 1;
+		else if (mas[gY][gX]==0) rez = 2;
+		else if (mas[gY][gX]>0) rez = 3;
+		
+		mas[gY][gX] = -2;
+		
+		return rez;
+	}
+	
+	void perem()
+	{
+		int flag = peremGoloa();
+		
+		if (flag == 3) endg = true;
+		
+		for (int i=0; i<30; i++)
+		{
+			for (int j=0; j<30; j++)
+				{
+					if (mas[i][j] > 0) mas[i][j]++;
+					else if (mas[i][j] == -2) mas[i][j]=1;
+					
+					if (flag!=1)
+					{
+						if (mas[i][j]==(dlina+1)) mas[i][j]=0;
+					}
+				}				
+		}
+		
+		if (flag == 1)
+		{
+			dlina++;
+			make_new();
+			kol+=10;
+		}
+		povorot();
+		
+	}
+	
+}
---
+++ src/snake/snake.java
@@ -0,0 +1,161 @@
+package snake;
+
+import javax.swing.*;
+import java.awt.*;
+import javax.imageio.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class snake {
+	public static void main(String[] args) {
+			new myFrame();
+	}
+}
+
+class myFrame extends JFrame
+{
+	public myFrame()
+	{
+		myPanel p = new myPanel();
+		Container cont = getContentPane();
+		cont.add(p);
+		setTitle("Snake");
+		setBounds(0,0,800,650);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
+
+class myPanel extends JPanel
+{
+	game myGame;
+	Timer tmDrow, tmUpdate;
+	Image fon, telo, golova,ob,endg;
+	JLabel lb;
+	JButton btn1, btn2;
+	myPanel pan;
+	
+	private class myKey implements KeyListener  
+	{
+		    // Метод, который срабатывает при нажатии
+	   	    public void keyPressed(KeyEvent e)
+	   	    {
+	   	    	// Получение кода нажатой клавиши
+	   	    	int key_ = e.getKeyCode();
+	   	    	if (key_==KeyEvent.VK_KP_LEFT) myGame.new_napr = 0;
+	   	    	if (key_==KeyEvent.VK_KP_RIGHT) myGame.new_napr = 2;
+	   	    	if (key_==KeyEvent.VK_KP_UP) myGame.new_napr = 1;
+	   	    	if (key_==KeyEvent.VK_KP_DOWN) myGame.new_napr = 3;
+	   	    	
+	   	    }
+	   	    public void keyReleased(KeyEvent e) {}
+	   	    public void keyTyped(KeyEvent e) {}
+	   }
+	
+	public myPanel()
+	{
+		pan = this;
+		addKeyListener(new myKey());
+		setFocusable(true);
+		
+		myGame = new game();
+		try 
+		{
+			fon=ImageIO.read(new File("./fon.png"));
+			telo=ImageIO.read(new File("./telo.png"));
+			golova=ImageIO.read(new File("./golova.png"));
+			ob=ImageIO.read(new File("./ob.png"));
+			endg=ImageIO.read(new File("./endg.png"));
+		} 
+		catch (Exception e) {}
+		
+		tmDrow = new Timer(20,new ActionListener() {				
+			public void actionPerformed(ActionEvent e) 
+			{
+	           repaint();
+			}
+		});
+		tmDrow.start();
+	   
+	   tmUpdate = new Timer(20,new ActionListener() {				
+			public void actionPerformed(ActionEvent e) {
+				if (myGame.endg==false)
+				{
+					myGame.perem();
+				}
+				lb.setText("Score: "+myGame.kol);
+	        }
+		});
+		tmUpdate.start();
+
+		setLayout(null);
+		
+		lb = new JLabel("Score: 0");
+		lb.setForeground(Color.WHITE);
+		lb.setFont(new Font("arial", 0, 20));
+		lb.setBounds(630, 200, 150, 50);
+		add(lb);
+		
+		btn1 = new JButton();
+		btn1.setText("NEW GAME");
+		btn1.setForeground(Color.BLUE);
+		btn1.setBounds(630,30,150,50);
+		btn1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myGame.start();
+				btn1.setFocusable(false);
+				btn2.setFocusable(false);
+				pan.setFocusable(true);
+			}
+		});
+		add(btn1);
+		
+		btn2 = new JButton();
+		btn2.setText("EXIT");
+		btn2.setForeground(Color.RED);
+		btn2.setBounds(630,100,150,50);
+		btn2.addActionListener(new ActionListener() {	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		add(btn2);
+		
+	}
+	
+	public void paintComponent(Graphics gr)
+	{
+		super.paintComponent(gr);
+		gr.drawImage(fon, 0, 0, null);
+		
+		for (int i=0; i<30; i++)
+		{
+			for (int j = 0; j<30; j++) 
+			{
+				if (myGame.mas[i][j]==1)
+					gr.drawImage(golova, 10+j*20, 10+i*20, null);
+				if (myGame.mas[i][j]==-1)
+					gr.drawImage(ob, 10+j*20, 10+i*20, null);
+				if (myGame.mas[i][j]>=2)
+					gr.drawImage(telo, 10+j*20, 10+i*20, null);
+				gr.setColor(Color.ORANGE);
+				gr.setFont(new Font("arial",0,22));
+				gr.drawString(""+myGame.mas[i][j], 12+j*20, 30+i*20);
+			}
+		}
+		
+		gr.setColor(Color.BLUE);
+		for (int i=0; i<30; i++)
+		{
+			gr.drawLine(10+i*20, 10, 10+i*20, 610);
+			gr.drawLine(10, 10+i*20, 610, 10+i*20);
+		}
+		
+		if (myGame.endg == true)
+			gr.drawImage(endg, 250, 200, 200, 100, null);
+	}
+	
+}
